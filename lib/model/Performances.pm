8c1e63376ce0fdede0238366d43687c18a552ea5
victoria <vrbb@github.com>
2022-04-20 12:05:43 -0300

    issue-01 Refactor - Creating constants for play types

--- lib/model/Performances.pm
+++ lib/model/Performances.pm
@@ -1,13 +1,20 @@
 package model::Performances; # General
 use strict; # Error readability
 
+use constant # Creating constants for play types
+    TYPE_PLAY => {
+        "tragedy" => 1,
+        "comedy" => 2
+    };
 
 sub new { # Constructor
     my $this = shift;
     my $class = ref ($this) || $this;
+    # Attributes in common across classes
     my $self = {
         name => shift,
         audience => shift,
+        type_play => shift,
     };
 
     return bless $self, $class;
@@ -29,15 +36,12 @@ sub new { # Constructor
     my $name = shift;
     my $audience = shift;
     
-    my $self = new Performances($name,$audience);
+    my $self = new Performances($name,$audience,TYPE_PLAY->{tragedy});
 
     return bless $self, $class;
 }
 
 
-sub amount_for { # Method
-    my $self = shift; 
-}
 
 package model::Comedy; # Class
 use strict; # Error readability
@@ -50,11 +54,8 @@ sub new {  # Constructor
     my $name = shift;
     my $audience = shift;
     
-    my $self = new Performances($name,$audience);
+    my $self = new Performances($name,$audience,TYPE_PLAY->{comedy});
 
     return bless $self, $class;
 }
 
-sub amount_for { # Method
-    my $self = shift; 
-}
